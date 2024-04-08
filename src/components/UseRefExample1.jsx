891c1233bba86c7d855c4267912fb1165394de15
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-08 19:17:30 +0600

    add a form for use-ref example-1

--- src/components/UseRefExample1.jsx
+++ src/components/UseRefExample1.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import { useRef } from 'react';
 
 const UseRefExample1 = () => {
   return (
-    <div>
-      <h1>UseRef Exampla 1</h1>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-6 mx-md-auto'>
+          <div className='py-5'>
+            <form className='d-flex flex-column gap-3'>
+              <div className='d-flex flex-column gap-1'>
+                <label htmlFor='name'>Enter Name</label>
+                <input
+                  type='text'
+                  id='name'
+                  className='form-control'
+                  placeholder='Enter Name'
+                />
+              </div>
+
+              <button type='submit' className='btn btn-primary'>
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
