32c65ca8b986f145402f474709fa55511d7c65ba
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-17 12:44:07 +0600

    setValue function done

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,7 +1,28 @@
 import { useState } from 'react';
 
-const useLocalStorage = () => {
+const useLocalStorage = (key, initialValue) => {
+  const [localStorageValue, setLocalStorageValue] = useState(() =>
+    getLocalStorageValue(key, initialValue)
+  );
+
+  const setValue = (value) => {
+    // CHECK IF-FUNCTION
+    const valueToStore =
+      value instanceof Function ? value(localStorageValue) : value;
+
+    // SET TO-STATE
+    setLocalStorageValue(value);
+    // SET TO-LOCAL-STORAGE
+    localStorage.setItem(key, JSON.stringify(valueToStore));
+  };
+
   return [localStorageValue, setValue];
 };
+
+function getLocalStorageValue(key, initialValue) {
+  const itemFromStorage = localStorage.getItem(key);
+
+  return itemFromStorage ? JSON.parse(itemFromStorage) : initialValue;
+}
 
 export default useLocalStorage;
