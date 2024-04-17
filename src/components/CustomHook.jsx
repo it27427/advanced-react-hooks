91601f7e31842f32c603b9a24a9ca6120d51b4e0
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-17 12:12:33 +0600

    add jsonplaceholder-url

--- src/components/CustomHook.jsx
+++ src/components/CustomHook.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import useFetch from '../hooks/useFetch';
 
 const CustomHook = () => {
+  const res = useFetch('https://jsonplaceholder.typicode.com//posts', {});
+
+  console.log(res);
+
   return (
     <div>
       <h2> data</h2>
