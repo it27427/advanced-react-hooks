a2510830dcf96f8d181f88065ffe21605f62d122
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-17 12:18:52 +0600

    CustomHook Component Markup and Data Fatching Completed

--- src/components/CustomHook.jsx
+++ src/components/CustomHook.jsx
@@ -1,13 +1,30 @@
 import useFetch from '../hooks/useFetch';
 
 const CustomHook = () => {
-  const res = useFetch('https://jsonplaceholder.typicode.com//posts', {});
+  const { data, loading, error } = useFetch(
+    'https://jsonplaceholder.typicode.com//posts',
+    {}
+  );
 
-  console.log(res);
+  if (loading) {
+    return <h3>Loading...</h3>;
+  }
 
   return (
-    <div>
-      <h2> data</h2>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-6 mx-md-auto'>
+          <div className='py-5 d-flex flex-column gap-3'>
+            {data.map((post) => (
+              <div className='card p-3'>
+                <h5 key={post.id} className='card-title'>
+                  {post.title}
+                </h5>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
