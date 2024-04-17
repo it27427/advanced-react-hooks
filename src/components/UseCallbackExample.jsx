423c39bc0ce29126e54945183290710f8d77a22b
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-17 11:42:59 +0600

    Component Markup done

--- src/components/UseCallbackExample.jsx
+++ src/components/UseCallbackExample.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const UseCallbackExample = () => {
   return (
-    <div>
-      <h2>UseCallbackExample</h2>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-6 mx-md-auto'>
+          <div className='py-5'>
+            <h2>UseCallbackExample</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
