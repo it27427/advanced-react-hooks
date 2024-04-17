ec7b4605785083251aa6a1877ce291e1406bebe9
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-17 11:53:27 +0600

    React.memo added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Button = ({ addTask }) => {
+const Button = React.memo(({ addTask }) => {
   return (
     <button onClick={addTask} type='button' className='btn btn-primary'>
       Add Task
     </button>
   );
-};
+});
 
 export default Button;
